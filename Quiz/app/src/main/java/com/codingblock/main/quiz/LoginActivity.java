52ef67be659e0d25711a8774fe2868ff4850ae97
Kartik Gupta <kartikgupta676@gmail.com>
2018-02-13 14:15:12 +0530

    Finalising Signup Activity

--- Quiz/app/src/main/java/com/codingblock/main/quiz/LoginActivity.java
+++ Quiz/app/src/main/java/com/codingblock/main/quiz/LoginActivity.java
@@ -1,18 +1,32 @@
 package com.codingblock.main.quiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
+    AppCompatButton mAppCompatButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAppCompatButton=findViewById(R.id.not_a_member_signup_button);
+
+        mAppCompatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
